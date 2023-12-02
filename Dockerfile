969023a164f97d9df24070f719ad6ec6239c8277
Kali Hale <kalihale@outlook.com>
2023-12-01 20:15:39 -0800

    Set up code for training

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+# Use Ubuntu 22.04
+FROM ubuntu:22.04
+RUN apt-get update && apt-get install -y libpq-dev gcc
+RUN pip3 install
+RUN pip3 install torch torchvision torchaudio torchtext --index-url https://download.pytorch.org/whl/cu118
+RUN pip3 install transformers
+RUN python3 -c "from transformers import pipeline; print(pipeline('sentiment-analysis')('we love you'))"
+RUN python3 main.py
--- main.py
+++ main.py
@@ -1,1 +1,49 @@
+from transformers import Trainer, TrainingArguments, AutoTokenizer, AutoModelForSequenceClassification
+from datasets import load_dataset
+
+
+# def preprocess(examples):
+#     inputs = tokenizer(
+#         examples['question'],
+#         examples['document'],
+#         padding='max_length',
+#         truncation=True,
+#         return_tensors='pt'
+#     )
+#     targets = tokenizer(
+#
+#     )
+
+
+# Load model
+model_name = "xyma/COSTA-wiki"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+dataset = load_dataset("natural_questions")
+dataset = dataset.shuffle(seed=37)
+
+train_data = dataset["train"]
+val_data = dataset["validation"]
+
+print(train_data['long_answer_candidates'])
+
+training_args = TrainingArguments(output_dir="./finetuned_model",
+                                  num_train_epochs=3,
+                                  per_device_train_batch_size=8,
+                                  per_device_eval_batch_size=8,
+                                  warmup_steps=500,
+                                  weight_decay=0.01,
+                                  logging_dir="./logs")
+
+trainer = Trainer(model=model,
+                  args=training_args,
+                  train_dataset=train_data,
+                  eval_dataset=val_data)
+
+trainer.train()
+
+results = trainer.evaluate()
+
+print(results)
 
