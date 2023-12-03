62c48525e4816f645436ddccfca7aeef08f9677d
Setareh <131782675+peakylogic@users.noreply.github.com>
2023-12-02 22:30:34 -0800

    initial training

--- training.py
+++ training.py
@@ -1,111 +1,131 @@
 import torch
 from torch.utils.data import DataLoader
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering, AdamW
+from transformers import AutoTokenizer, AutoModel, AdamW
 from datasets import load_dataset
+import torch.nn.functional as F
 
-# Load and preprocess the SQuAD dataset
+def compute_loss(start_logits, end_logits, start_positions, end_positions):
+    # Cross-entropy loss for start and end positions
+    start_loss = F.cross_entropy(start_logits, start_positions)
+    end_loss = F.cross_entropy(end_logits, end_positions)
+    
+    # The total loss is the average of the start and end losses
+    total_loss = (start_loss + end_loss) / 2
+    return total_loss
+
+
+# Custom COSTA Model for Question Answering
+class COSTAForQA(torch.nn.Module):
+    def __init__(self, model_name):
+        super().__init__()
+        self.costa_encoder = AutoModel.from_pretrained(model_name)
+        self.qa_outputs = torch.nn.Linear(self.costa_encoder.config.hidden_size, 2)
+
+    def forward(self, input_ids, attention_mask):
+        sequence_output = self.costa_encoder(input_ids=input_ids, attention_mask=attention_mask)[0]
+        qa_logits = self.qa_outputs(sequence_output)
+        start_logits, end_logits = qa_logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+        return start_logits, end_logits
+
+# Load the dataset
 dataset = load_dataset("squad")
 
+# Specify the tokenizer and model name
 model_name = "xyma/COSTA-wiki"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# Preprocessing function for the dataset
+# Preprocessing function for tokenization
 def preprocess_function(examples):
     tokenized_examples = tokenizer(
         examples["question"],
         examples["context"],
-        truncation="only_second",  # Truncate only the context part
         max_length=512,
-        stride=128,
-        return_overflowing_tokens=True,
+        truncation="only_second",
         return_offsets_mapping=True,
-        padding="max_length"
+        padding="max_length",
     )
-
-    # Extract start and end positions of answers
-    sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+    offset_mapping = tokenized_examples.pop("offset_mapping")
     answers = examples["answers"]
-    tokenized_examples["start_positions"] = []
-    tokenized_examples["end_positions"] = []
+    start_positions = []
+    end_positions = []
 
-    for i, offsets in enumerate(tokenized_examples["offset_mapping"]):
-        cls_index = tokenizer.cls_token_id
+    for i, offsets in enumerate(offset_mapping):
+        answer = answers[i]
+        start_char = answer["answer_start"][0]
+        end_char = start_char + len(answer["text"][0])
         sequence_ids = tokenized_examples.sequence_ids(i)
 
-        # If no answers are provided, set the cls index as answer
-        if len(answers[i]["answer_start"]) == 0:
-            tokenized_examples["start_positions"].append(cls_index)
-            tokenized_examples["end_positions"].append(cls_index)
-        else:
-            # Extract start and end positions in the token space
-            start_char = answers[i]["answer_start"][0]
-            end_char = start_char + len(answers[i]["text"][0])
-            token_start_index = 0
-            token_end_index = len(input_ids) - 1
-            while sequence_ids[token_start_index] != 1:
-                token_start_index += 1
-            while sequence_ids[token_end_index] != 1:
-                token_end_index -= 1
-            tokenized_examples["start_positions"].append(token_start_index)
-            tokenized_examples["end_positions"].append(token_end_index)
+        context_start = 0
+        while sequence_ids[context_start] != 1:
+            context_start += 1
+        context_end = len(sequence_ids) - 1
+        while sequence_ids[context_end] != 1:
+            context_end -= 1
 
+        if offsets[context_start][0] > end_char or offsets[context_end][1] < start_char:
+            start_positions.append(0)
+            end_positions.append(0)
+        else:
+            start_token = context_start
+            while start_token < len(offsets) and offsets[start_token][0] <= start_char:
+                start_token += 1
+            end_token = context_end
+            while end_token >= context_start and offsets[end_token][1] >= end_char:
+                end_token -= 1
+
+            start_positions.append(start_token - 1)
+            end_positions.append(end_token + 1)
+
+    tokenized_examples["start_positions"] = start_positions
+    tokenized_examples["end_positions"] = end_positions
     return tokenized_examples
 
+# Tokenize the dataset
 tokenized_datasets = dataset.map(preprocess_function, batched=True, remove_columns=dataset["train"].column_names)
 
-# Load the COSTA model
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+# Define a custom collate function for the DataLoader
+def collate_fn(batch):
+    return {
+        'input_ids': torch.stack([torch.tensor(item['input_ids'], dtype=torch.long) for item in batch]),
+        'attention_mask': torch.stack([torch.tensor(item['attention_mask'], dtype=torch.long) for item in batch]),
+        'start_positions': torch.stack([torch.tensor(item['start_positions'], dtype=torch.long) for item in batch]),
+        'end_positions': torch.stack([torch.tensor(item['end_positions'], dtype=torch.long) for item in batch]),
+    }
 
-# Create DataLoaders
-train_loader = DataLoader(tokenized_datasets["train"], batch_size=16, shuffle=True)
-val_loader = DataLoader(tokenized_datasets["validation"], batch_size=16)
+# Create DataLoaders with the custom collate function
+train_loader = DataLoader(tokenized_datasets["train"], batch_size=16, shuffle=True, collate_fn=collate_fn)
+val_loader = DataLoader(tokenized_datasets["validation"], batch_size=16, collate_fn=collate_fn)
+
+# Initialize the COSTA model for QA
+model = COSTAForQA(model_name)
+
+# Move model to GPU if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 
 # Define the optimizer
 optimizer = AdamW(model.parameters(), lr=5e-5)
 
-# Training Loop
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-model.to(device)
-
-for epoch in range(3):  # Example: 3 epochs
+# Training loop
+for epoch in range(3):  # Adjust the number of epochs as needed
     model.train()
     for batch in train_loader:
-        # Move batch data to the device
         input_ids = batch['input_ids'].to(device)
         attention_mask = batch['attention_mask'].to(device)
         start_positions = batch['start_positions'].to(device)
         end_positions = batch['end_positions'].to(device)
 
-        # Forward pass
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask,
-                        start_positions=start_positions, end_positions=end_positions)
-        loss = outputs.loss
-
-        # Backward pass and optimization
         optimizer.zero_grad()
+        start_logits, end_logits = model(input_ids, attention_mask)
+        loss = compute_loss(start_logits, end_logits, start_positions, end_positions)
         loss.backward()
         optimizer.step()
 
-        print(f"Epoch {epoch} Loss {loss.item()}")
+        print(f"Epoch {epoch}, Loss: {loss.item()}")
 
-    # Validation Loop
-    model.eval()
-    total_eval_loss = 0
-    for batch in val_loader:
-        input_ids = batch['input_ids'].to(device)
-        attention_mask = batch['attention_mask'].to(device)
-        start_positions = batch['start_positions'].to(device)
-        end_positions = batch['end_positions'].to(device)
+    # Validation step (if necessary)
 
-        with torch.no_grad():
-            outputs = model(input_ids=input_ids, attention_mask=attention_mask,
-                            start_positions=start_positions, end_positions=end_positions)
-
-        loss = outputs.loss
-        total_eval_loss += loss.item()
-
-    avg_val_loss = total_eval_loss / len(val_loader)
-    print(f"Validation Loss: {avg_val_loss}")
-
-# Save the model
+# Save the fine-tuned model
 model.save_pretrained("./costa_finetuned")
